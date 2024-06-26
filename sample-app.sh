9fbe7894604c0dd7645df8c86d4c73717beee8f7
re1nhart01 <eugene.kokaiko@gmail.com>
2024-06-26 01:34:10 +0000

    change port to 8080

--- sample-app.sh
+++ sample-app.sh
@@ -12,3 +12,8 @@ echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 8080" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
+cd tempdir
+docker build -t sampleapp .
+docker run -t -d -p 8080:8080 --name samplerunning sampleapp
+docker ps -a
+
