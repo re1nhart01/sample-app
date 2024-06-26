a45c07a1dc3be801f55af4684c67bd4a73ea1e2b
re1nhart01 <eugene.kokaiko@gmail.com>
2024-06-26 02:06:33 +0000

    change port to 8080

--- sample-app.sh
+++ sample-app.sh
@@ -14,6 +14,6 @@ echo "EXPOSE 8080" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
 cd tempdir
 docker build -t sampleapp .
-docker run -t -d -p 8080:8080 --name samplerunning sampleapp
+docker run -t -d -p 5050:5050 --name samplerunning sampleapp
 docker ps -a
 
