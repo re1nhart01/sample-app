03fc0e09066bc24d04e072d43fb002e5cfe8b061
re1nhart01 <eugene.kokaiko@gmail.com>
2024-06-26 02:07:55 +0000

    change port to 8080

--- sample_app.py
+++ sample_app.py
@@ -8,4 +8,4 @@ sample = Flask(__name__)
 def main():
      return render_template("index.html")
 if __name__ == "__main__":
-    sample.run(host="0.0.0.0", port=8080)
+    sample.run(host="0.0.0.0", port=5050)
